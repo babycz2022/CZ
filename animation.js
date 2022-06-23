d0a84bf82172896f9ed79245b38325c23cb40bab
dr-ishigami-senku <nu522696@gmail.com>
2022-06-23 22:34:29 +0800

    😁

--- animation.js
+++ animation.js
@@ -1,99 +1,43 @@
-//Tokenomics Cards animation
-const io1 = new IntersectionObserver(entries => {
-entries.forEach(entry => {
-  if (entry.isIntersecting) {
-    entry.target.classList.add('green-card-uno');
-    entry.target.classList.add('green-card-dos');
-    entry.target.classList.add('red-card-uno');
-    entry.target.classList.add('red-card-dos');
-    entry.target.classList.add('red-card-tres');
-  }
-  else {
-    entry.target.classList.remove('green-card-uno');
-    entry.target.classList.remove('green-card-dos');
-    entry.target.classList.remove('red-card-uno');
-    entry.target.classList.remove('red-card-dos');
-    entry.target.classList.remove('red-card-tres');
-  }
-})
-})
-const boxElList1 = document.querySelectorAll('.card');
-boxElList1.forEach((el) => {
-io1.observe(el);
-})
+function scrollTrigger(selector, options = {}){
+    let els = document.querySelectorAll(selector)
+    els = Array.from(els)
+    els.forEach(el => {
+        addObserver(el, options)
+    })
+}
 
-//Team Members animation
-//1st Member
-const io2 = new IntersectionObserver(entries => {
-entries.forEach(entry => {
-  if (entry.isIntersecting) {
-    entry.target.classList.add('rise-animation1');
-
-  }
-  else {
-    entry.target.classList.remove('rise-animation1');
-
-  }
-})
-})
-const boxElList2 = document.querySelectorAll('.rise-animation1');
-boxElList2.forEach((el) => {
-io2.observe(el);
-})
-
-//2nd Member
-const io3 = new IntersectionObserver(entries => {
-entries.forEach(entry => {
-  if (entry.isIntersecting) {
-
-    entry.target.classList.add('rise-animation2');
-  }
-  else {
-
-    entry.target.classList.remove('rise-animation2');
-  }
-})
-})
-const boxElList3 = document.querySelectorAll('.rise-animation2');
-boxElList3.forEach((el) => {
-io3.observe(el);
-})
-
-//3rd Member
-const io4 = new IntersectionObserver(entries => {
-entries.forEach(entry => {
-  if (entry.isIntersecting) {
-    entry.target.classList.add('rise-animation3');
-
-  }
-  else {
-    entry.target.classList.remove('rise-animation3');
-
-  }
-})
-})
-const boxElList4 = document.querySelectorAll('.rise-animation3');
-boxElList4.forEach((el) => {
-io4.observe(el);
-})
-
-//4th Member
-const io5 = new IntersectionObserver(entries => {
-entries.forEach(entry => {
-  if (entry.isIntersecting) {
-
-    entry.target.classList.add('rise-animation4');
-  }
-  else {
-
-    entry.target.classList.remove('rise-animation4');
-  }
-})
-})
-const boxElList5 = document.querySelectorAll('.rise-animation4');
-boxElList5.forEach((el) => {
-io5.observe(el);
-})
+function addObserver(el, options){
+    if(!('IntersectionObserver' in window)){
+        if(options.cb){
+            options.cb(el)
+        }else{
+            entry.target.classList.add('active')
+        }
+        return
+    }
+    let observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting){
+                if(options.cb){
+                    options.cb(el)
+                }else{
+                    entry.target.classList.add('active')
+                }
+                observer.unobserve(entry.target)
+            }
+        })
+    }, options)
+    observer.observe(el)
+}
+//About Section animation
+scrollTrigger('.about-img')
+scrollTrigger('.col-right')
+scrollTrigger('.about-2')
+scrollTrigger('.about-3')
+//Tokenomics Card animation
+scrollTrigger('.card')
+//Team Section Members animation
+scrollTrigger('.team-member')
 
 //Twitter Logo Hover animation
 var image = document.getElementById("twt-hover");
